968754a73eab7d6d4bccf8ca27558435cd3d8825
Gautam1124 <108386705+Gautam1124@users.noreply.github.com>
2023-02-01 10:32:29 +0530

    Update flip.py

--- assignment_3/flip.py
+++ assignment_3/flip.py
@@ -5,12 +5,13 @@ class FlipImage(object):
     '''
         Flips the image.
     '''
-
+    __slots__ = "_prop","_imageR"
     def __init__(self, flip_type='horizontal'):
         '''
             Arguments:
             flip_type: 'horizontal' or 'vertical' Default: 'horizontal'
         '''
+        self._prop = flip_type
 
     def __call__(self, image):
         '''
@@ -19,3 +20,8 @@ class FlipImage(object):
             Returns:
             image (numpy array or PIL image)
         '''
+        if self._prop == "horizontal" :
+            self._imageR = image.transpose(method=Image.FLIP_LEFT_RIGHT)
+        else :
+            self._imageR = image.transpose(method=Image.FLIP_TOP_BOTTOM)
+        return self._imageR
