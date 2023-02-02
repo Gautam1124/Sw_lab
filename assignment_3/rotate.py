ad33181bee8928687a8dd2d62048bd174fb1d6b7
Gautam1124 <108386705+Gautam1124@users.noreply.github.com>
2023-02-02 20:24:39 +0530

    Update rotate.py

--- assignment_3/rotate.py
+++ assignment_3/rotate.py
@@ -5,12 +5,13 @@ class RotateImage(object):
     '''
         Rotates the image about the centre of the image.
     '''
-
+    __slots__ = "_deg"
     def __init__(self, degrees):
         '''
             Arguments:
             degrees: rotation degree.
         '''
+        self._deg = degrees
 
     def __call__(self, sample):
         '''
@@ -19,3 +20,5 @@ class RotateImage(object):
             Returns:
             image (numpy array or PIL image)
         '''
+        self._img = sample.rotate(self._deg, PIL.Image.NEAREST, expand = 1)
+        return self._img
