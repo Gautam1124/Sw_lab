84270ac212bf01490060b4a78b1fc4b9cc08023f
Gautam1124 <108386705+Gautam1124@users.noreply.github.com>
2023-02-02 12:43:05 +0530

    Update rescale.py

--- assignment_3/rescale.py
+++ assignment_3/rescale.py
@@ -5,6 +5,7 @@ class RescaleImage(object):
     '''
         Rescales the image to a given size.
     '''
+    __slots__ = "_wid", "_hgh"
 
     def __init__(self, output_size):
         '''
@@ -13,6 +14,7 @@ class RescaleImage(object):
             matched to output_size. If int, smaller of image edges is matched
             to output_size keeping aspect ratio the same.
         '''
+        self._wid , self._hgh = output_size
 
 
     def __call__(self, image):
@@ -23,3 +25,5 @@ class RescaleImage(object):
             image (numpy array or PIL image)
             Note: You do not need to resize the bounding boxes. ONLY RESIZE THE IMAGE.
         '''
+        image_scale = image.resize(self._wid,self._hgh)
+        return image_scale
